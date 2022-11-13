1344133c0ad04d3e90f8f057c5e8a7a0ab763e2a
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-11-13 14:36:06 +0530

    Add files via upload

--- desk.js
+++ desk.js
@@ -1,10 +1,30 @@
+var img="";
+var status="";
 function preload(){
     img = loadImage("desk.jpg");
     }
     function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector= ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded(){
+console.log("cocossd Model Loaded");
+status= true;
+objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+}
     }
-    function draw(){
+ function draw(){
     image(img, 0, 0, 640, 420);
+    }
+function back(){
+        window.location.href = "https://zahra1205.github.io/OBJECT_DETECTION/";
     }
